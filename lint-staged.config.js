43e3e337e164d3bca3a6a685cd9f9de5ecd0e1c1
Jose Robinson <dev@joserobinson.com>
2023-11-16 20:56:13 -0500

    chore: add lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,12 @@
+const path = require('node:path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+module.exports = {
+  '*.{js,ts,tsx}': [buildEslintCommand],
+  '*.{ts,tsx}': () => ['yarn run check-types'],
+  '*.{json,md}': ['prettier --write'],
+};
